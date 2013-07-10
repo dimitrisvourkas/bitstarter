bac7da8d8aa688d313ba573a9e7910b0b185f598
Dimitrios Vourkas <dimitris.vourkas@gmail.com>
2013-07-10 14:57:46 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var fs = require('fs');
 
-var app = express.createServer(express. app.get('/', function(request, response) {
-    
-    var dt=data.toString("utf-8",0,27);
-    response.send( });
+var app = express.createServer(express.logger());
+fs.readFile('index.html', function (err, data){
+   if (err) throw err;
+     
+    app.get('/', function(request, response) {
+//var buf= new Buffer(20);
+//buf.write("data","utf-8");
+var dt=data.toString("utf-8",0,27);
+response.send(dt);
+ });
 });
 var port = process.env.PORT || 5000;
-app.listen( console.log("Listening on " + port);
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
-port, function() {
-dt);
-logger());
- fs. readFile(' index. 
-html', if ( err) throw err;
-function ( err, data){
