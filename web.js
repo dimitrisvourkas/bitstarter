500ba2a1ac38b6416119c90e26e0fbcab300a03f
Dimitrios Vourkas <dimitris.vourkas@gmail.com>
2013-07-10 18:11:00 +0000

    Last change web.js

--- web.js
+++ web.js
@@ -6,13 +6,12 @@ fs.readFile('index.html', function (err, data){
    if (err) throw err;
      
     app.get('/', function(request, response) {
-//var buf= new Buffer(20);
-//buf.write("data","utf-8");
-var dt=data.toString("utf-8",0,27);
-response.send(dt);
+	var dt=data.toString("utf-8",0,27);
+	response.send(dt);
  });
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
